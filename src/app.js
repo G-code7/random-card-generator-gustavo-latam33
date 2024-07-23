201d2e2a09fa7c6368bb643763756cffaccbe692
Gustavo L <95352419+G-code7@users.noreply.github.com>
2024-07-23 02:39:04 +0000

    random cards generator complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,64 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const suits = ["hearts", "diamonds", "clubs", "spades"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+const suitIcons = {
+  hearts: "♥",
+  diamonds: "♦",
+  clubs: "♠",
+  spades: "♣"
 };
+
+function createDeck() {
+  let deck = [];
+  for (let suit of suits) {
+    for (let value of values) {
+      deck.push({ suit: suit, value: value });
+    }
+  }
+  return deck;
+}
+
+function drawRandomCard(deck) {
+  const randomIndex = Math.floor(Math.random() * deck.length);
+  const card = deck[randomIndex];
+  deck.splice(randomIndex, 1);
+  return card;
+}
+
+function generateRandomCard() {
+  let deck = createDeck();
+  let randomCard = drawRandomCard(deck);
+  document.getElementById("card-value").textContent = randomCard.value;
+
+  let suitRandom = suitIcons[randomCard.suit];
+  let suitElements = document.querySelectorAll(".card-suit");
+  suitElements[0].innerHTML = suitRandom;
+  suitElements[1].innerHTML = suitRandom;
+
+  // Cambiar el color del valor y el palo según el palo
+  let cardElement = document.getElementById("card");
+  if (randomCard.suit === "hearts" || randomCard.suit === "diamonds") {
+    cardElement.style.color = "red";
+  } else {
+    cardElement.style.color = "black";
+  }
+}
+
+window.onload = generateRandomCard;
+console.log("Hello Rigo from the console!");
